8d2c2e4ca48f5678f0debdf8f38a2e8426bb5c28
Hunter Perrin <hperrin@gmail.com>
2014-12-17 15:23:47 -0800

    Better Travis DB definition.

--- testing/bootstrapMySQL.php
+++ testing/bootstrapMySQL.php
@@ -7,11 +7,19 @@ RPHP::_('NymphConfig', array(), function(){
 	// Nymph's configuration.
 
 	$nymph_config = include(dirname(__FILE__).DIRECTORY_SEPARATOR.'../conf/defaults.php');
-
-	$nymph_config->MySQL->host['value'] = '127.0.0.1';
-	$nymph_config->MySQL->database['value'] = 'nymph_testing';
-	$nymph_config->MySQL->user['value'] = 'nymph_testing';
-	$nymph_config->MySQL->password['value'] = 'password';
+	if (getenv('DATABASE_URL')) {
+		$dbopts = parse_url(getenv('DATABASE_URL'));
+		$nymph_config->MySQL->database['value'] = ltrim($dbopts["path"],'/');
+		$nymph_config->MySQL->host['value'] = $dbopts["host"];
+		$nymph_config->MySQL->port['value'] = $dbopts["port"];
+		$nymph_config->MySQL->user['value'] = $dbopts["user"];
+		$nymph_config->MySQL->password['value'] = $dbopts["pass"];
+	} else {
+		$nymph_config->MySQL->host['value'] = '127.0.0.1';
+		$nymph_config->MySQL->database['value'] = 'nymph_testing';
+		$nymph_config->MySQL->user['value'] = 'nymph_testing';
+		$nymph_config->MySQL->password['value'] = 'password';
+	}
 
 	return $nymph_config;
 });
--- testing/bootstrapPostgreSQL.php
+++ testing/bootstrapPostgreSQL.php
@@ -8,10 +8,19 @@ RPHP::_('NymphConfig', array(), function(){
 
 	$nymph_config = include(dirname(__FILE__).DIRECTORY_SEPARATOR.'../conf/defaults.php');
 
-	$nymph_config->driver['value'] = 'PostgreSQL';
-	$nymph_config->PostgreSQL->database['value'] = 'nymph_testing';
-	$nymph_config->PostgreSQL->user['value'] = 'nymph_testing';
-	$nymph_config->PostgreSQL->password['value'] = 'password';
+		$nymph_config->driver['value'] = 'PostgreSQL';
+	if (getenv('DATABASE_URL')) {
+		$dbopts = parse_url(getenv('DATABASE_URL'));
+		$nymph_config->PostgreSQL->database['value'] = ltrim($dbopts["path"],'/');
+		$nymph_config->PostgreSQL->host['value'] = $dbopts["host"];
+		$nymph_config->PostgreSQL->port['value'] = $dbopts["port"];
+		$nymph_config->PostgreSQL->user['value'] = $dbopts["user"];
+		$nymph_config->PostgreSQL->password['value'] = $dbopts["pass"];
+	} else {
+		$nymph_config->PostgreSQL->database['value'] = 'nymph_testing';
+		$nymph_config->PostgreSQL->user['value'] = 'nymph_testing';
+		$nymph_config->PostgreSQL->password['value'] = 'password';
+	}
 
 	return $nymph_config;
 });
