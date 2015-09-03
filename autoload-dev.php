15a68e238b4ee34c83a346670c63629e7a6fbdf5
Hunter Perrin <hperrin@gmail.com>
2015-09-02 17:31:13 -0700

    Updated to all new versions. 1.4.3.

--- autoload-dev.php
+++ autoload-dev.php
@@ -1,11 +1,15 @@
 <?php
 
-file_put_contents(__DIR__.'/vendor/composer/autoload_psr4.php', str_replace([
-	'array($vendorDir . \'/sciactive/nymph-pubsub/src\')',
-	'array($vendorDir . \'/sciactive/nymph-server/src\')',
-], [
-	'array(\''.__DIR__.'/pubsub/src\')',
-	'array(\''.__DIR__.'/server/src\')',
-], file_get_contents(__DIR__.'/vendor/composer/autoload_psr4.php')));
+$contents = file_get_contents(__DIR__.'/vendor/composer/autoload_psr4.php');
+
+if (strpos($contents, 'array($vendorDir . \'/sciactive/nymph-server/src\')') !== false) {
+	file_put_contents(__DIR__.'/vendor/composer/autoload_psr4.php', str_replace([
+		'array($vendorDir . \'/sciactive/nymph-pubsub/src\')',
+		'array($vendorDir . \'/sciactive/nymph-server/src\')',
+	], [
+		'array(\''.__DIR__.'/pubsub/src\')',
+		'array(\''.__DIR__.'/server/src\')',
+	], $contents));
+}
 
 require __DIR__.'/vendor/autoload.php';
