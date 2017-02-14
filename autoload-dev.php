fdc4abf873e6f5ed6c8181cc9c4a520ba94323c6
Hunter Perrin <hperrin@gmail.com>
2017-02-14 15:16:14 -0800

    Nymph PubSub is working now on PHP7.

--- autoload-dev.php
+++ autoload-dev.php
@@ -1,15 +1,44 @@
 <?php
 
-$contents = file_get_contents(__DIR__.'/vendor/composer/autoload_psr4.php');
+$files = [
+  __DIR__.'/vendor/composer/autoload_psr4.php',
+  __DIR__.'/vendor/composer/autoload_namespaces.php',
+  __DIR__.'/vendor/composer/autoload_static.php',
+];
 
-if (strpos($contents, 'array($vendorDir . \'/sciactive/nymph-server/src\')') !== false) {
-  file_put_contents(__DIR__.'/vendor/composer/autoload_psr4.php', str_replace([
-    'array($vendorDir . \'/sciactive/nymph-pubsub/src\')',
-    'array($vendorDir . \'/sciactive/nymph-server/src\')',
-  ], [
-    'array(\''.__DIR__.'/pubsub/src\')',
-    'array(\''.__DIR__.'/server/src\')',
-  ], $contents));
+foreach ($files as $file) {
+  $contents = file_get_contents($file);
+
+  if (strpos($contents, 'array($vendorDir . \'/sciactive/nymph-server/src\')')
+      !== false) {
+    $contents = str_replace(
+        [
+          'array($vendorDir . \'/sciactive/nymph-pubsub/src\')',
+          'array($vendorDir . \'/sciactive/nymph-server/src\')',
+        ],
+        [
+          'array(\''.__DIR__.'/pubsub/src\')',
+          'array(\''.__DIR__.'/server/src\')',
+        ],
+        $contents
+    );
+    file_put_contents($file, $contents);
+  }
+  if (strpos($contents, '__DIR__ . \'/..\' . \'/sciactive/nymph-server/src\'')
+      !== false) {
+    $contents = str_replace(
+        [
+          '__DIR__ . \'/..\' . \'/sciactive/nymph-pubsub/src\'',
+          '__DIR__ . \'/..\' . \'/sciactive/nymph-server/src\'',
+        ],
+        [
+          '\''.__DIR__.'/pubsub/src\'',
+          '\''.__DIR__.'/server/src\'',
+        ],
+        $contents
+    );
+    file_put_contents($file, $contents);
+  }
 }
 
 require __DIR__.'/vendor/autoload.php';
