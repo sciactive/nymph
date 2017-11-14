c66e1c8907dd4a174a3c21095435e1bc40871aaf
Hunter Perrin <hperrin@gmail.com>
2017-11-13 19:12:41 -0800

    Added Tilmeld.

--- autoload-dev.php
+++ autoload-dev.php
@@ -20,6 +20,18 @@ require __DIR__.'/vendor/autoload.php';
   foreach (glob(__DIR__.'/pubsub/src/*.php') as $file) {
     $classMap['Nymph\\PubSub\\'.basename($file, '.php')] = $file;
   }
+  foreach (glob(__DIR__.'/tilmeld/src/Entities/Mail/*.php') as $file) {
+    $classMap['Tilmeld\\Entities\\Mail\\'.basename($file, '.php')] = $file;
+  }
+  foreach (glob(__DIR__.'/tilmeld/src/Entities/*.php') as $file) {
+    $classMap['Tilmeld\\Entities\\'.basename($file, '.php')] = $file;
+  }
+  foreach (glob(__DIR__.'/tilmeld/src/Exceptions/*.php') as $file) {
+    $classMap['Tilmeld\\Exceptions\\'.basename($file, '.php')] = $file;
+  }
+  foreach (glob(__DIR__.'/tilmeld/src/*.php') as $file) {
+    $classMap['Tilmeld\\'.basename($file, '.php')] = $file;
+  }
   spl_autoload_register(function ($className) use ($classMap) {
     if (isset($classMap[$className]))
       include $classMap[$className];
